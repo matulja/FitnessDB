e6541e6ca8d32f44fe74a2419e0116aa37e04fdd
matulja <maryna.fehler@gmail.com>
2017-03-22 16:54:27 +0100

    DB migration from mysql to postgresql

--- src/main/java/fitness_db/CopyFitnessDBFomSqlToPostgre.java
+++ src/main/java/fitness_db/CopyFitnessDBFomSqlToPostgre.java
@@ -1,15 +1,57 @@
 package fitness_db;
 
+import de.akquinet.jbosscc.guttenbase.configuration.impl.GenericTargetDatabaseConfiguration;
+import de.akquinet.jbosscc.guttenbase.connector.DatabaseType;
+import de.akquinet.jbosscc.guttenbase.meta.DatabaseMetaData;
+import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
+import de.akquinet.jbosscc.guttenbase.repository.impl.ConnectorRepositoryImpl;
+import de.akquinet.jbosscc.guttenbase.tools.DefaultTableCopyTool;
+import de.akquinet.jbosscc.guttenbase.tools.DropTablesTool;
+import de.akquinet.jbosscc.guttenbase.tools.schema.CreateSchemaTool;
+
+import java.sql.SQLException;
+
+
 /**
  * Created by mfehler on 21.03.17.
  */
 public class CopyFitnessDBFomSqlToPostgre {
 
 
-    public static void main(final String[] args) {
+    public static final String SOURCE = "source";
+    public static final String TARGET = "target";
 
-        new MyPostgreConnetionsInfo();
+    public static void main(final String[] args) throws SQLException {
+
+        //Repository initialisieren
+        final ConnectorRepository connectorRepository = new ConnectorRepositoryImpl();
+        connectorRepository.addTargetDatabaseConfiguration(DatabaseType.POSTGRESQL, new GenericTargetDatabaseConfiguration(connectorRepository));
+
+        //add Source & Target ConnectionInfo
+        connectorRepository.addConnectionInfo(SOURCE, new MySqlConnectionsInfo());
+        connectorRepository.addConnectionInfo(TARGET, new MyPostgreConnetionsInfo());
+
+        //get Source Name
+        DatabaseMetaData source = connectorRepository.getDatabaseMetaData(SOURCE);
+        DatabaseMetaData target = connectorRepository.getDatabaseMetaData(TARGET);
+
+        System.out.println(source.getDatabaseName());
+        System.out.println(target.getDatabaseName());
 
 
+        //delete the tables
+
+        /*DropTablesTool dropTablesTool = new DropTablesTool(connectorRepository);
+        dropTablesTool.dropIndexes(TARGET);
+        dropTablesTool.dropForeignKeys(TARGET);
+        dropTablesTool.dropTables(TARGET);*/
+
+
+        //copy only Schema
+        new CreateSchemaTool(connectorRepository).copySchema(SOURCE, TARGET);
+
+
+        //copy Tables
+        new DefaultTableCopyTool(connectorRepository).copyTables(SOURCE,TARGET);
     }
 }
