b65d90d9bb141e5353610c4f8e4d078cac1f3d7b
matulja <maryna.fehler@gmail.com>
2017-03-22 12:26:10 +0100

    Initial commit

---
+++ src/main/java/fitness_db/CopyFitnessDBFomSqlToPostgre.java
@@ -0,0 +1,15 @@
+package fitness_db;
+
+/**
+ * Created by mfehler on 21.03.17.
+ */
+public class CopyFitnessDBFomSqlToPostgre {
+
+
+    public static void main(final String[] args) {
+
+        new MyPostgreConnetionsInfo();
+
+
+    }
+}
---
+++ src/main/java/fitness_db/MySqlConnections.java
@@ -0,0 +1,17 @@
+package fitness_db;
+
+import de.akquinet.jbosscc.guttenbase.connector.DatabaseType;
+import de.akquinet.jbosscc.guttenbase.connector.impl.URLConnectorInfoImpl;
+
+/**
+ * Created by mfehler on 21.03.17.
+ */
+class MySqlConnectionsInfo extends URLConnectorInfoImpl  {
+
+    private static final long serialVersionUID = 1L;
+
+        public MySqlConnectionsInfo() {
+          super("jdbc:mysql://localhost:3306/fitness", "root", "pass", "", "", DatabaseType.MYSQL);
+        }
+
+    }
